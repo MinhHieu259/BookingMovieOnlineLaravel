3abade4ab7fbc93ca8838e468020f2d8595a714a
MinhHieu259 <mhieu7252@gmail.com>
2023-05-22 08:12:34 +0700

    add url

--- resources/views/components/admin/trang-chu.blade.php
+++ resources/views/components/admin/trang-chu.blade.php
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Trang quản trị</h1>
+
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -17,6 +17,7 @@
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
+            <h1 class="text-center">Chào mừng đến trang quản trị</h1>
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
